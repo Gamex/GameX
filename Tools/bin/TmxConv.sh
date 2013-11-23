c96785e977c9544956c35be2a6b30109de58ade8
559210 <552910@qq.com>
2013-11-23 15:32:28 +0800

    remove an unuse file

--- Tools/bin/TmxConv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CURDir=$PWD;
-EXEFILE="$CURDir"/.
-OUTPUT_PATH="$CURDir"/../../GameX/
-INPUT_PATH="$CURDir"/../../GameX/Assets/GameX/TMX/
-WBTMXTOOL="$CURDir"/WBTMXTool
-
-$EXEFILE $INPUT_PATH $OUTPUT_PATH $WBTMXTOOL
